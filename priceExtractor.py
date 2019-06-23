6f25c9aa4738574761a1af6d5bb5d799dd324c96
ComradeAkko <jirojpn2412@gmail.com>
2019-06-22 20:48:56 -0400

    more progress in extracting prices

--- priceExtractor.py
+++ priceExtractor.py
@@ -1,8 +1,16 @@
 #priceExtractor by Jiro Mizuno
 
+#importing libraries
 import urllib
+import os
 
+#getting the url for the finance
 base = "http://ichart.finance.yahoo.com/table.csv?s="
 
+#concatenates current URL with ticker
 def concatURL(ticker):
     return base + ticker
+
+#get the current path and create a new path
+currPath = os.getcwd()
+outputPath = 
