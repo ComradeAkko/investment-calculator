a1ce7020b431aae71dbcabcdc3b48375048d8af6
ComradeAkko <jirojpn2412@gmail.com>
2019-06-25 18:47:41 -0400

    some updates to extractor

--- README.md
+++ README.md
@@ -6,6 +6,5 @@ This project was created to determine whether certain investment strategies were
 
 Pandas is already known to be a good data reader, but I want to try creating a data reader and analyzer from the ground up, so I won't be using it. 
 
-
 ## Disclaimer
 I'm definitely not by any means an experienced investor or a coder, so my results may not be entirely accurate. Furthermore, past market results generally are not indicative of any future results, so any historical data I find may not apply to the future. 
--- priceExtractor.py
+++ priceExtractor.py
@@ -4,17 +4,16 @@
 import urllib
 import os
 
-#getting the url for the finance
-base = "http://ichart.finance.yahoo.com/table.csv?s="
-
 #concatenates current URL with ticker
 def concatURL(ticker):
+    #getting the url for the finance
+	base = "http://ichart.finance.yahoo.com/table.csv?s="
     return base + ticker
 
 def makeFile(ticker,directory):
     currPath = os.getcwd()
-    directoryPath = currPath + "/" + directoryPath + "/"
-    return 
+    path = currPath + "/" + directory + "/" + ticker + ".csv"
+    return path
 
 def extractHistoricalPrices(ticker, directoryName):
     #get the current path and create a new path
@@ -22,4 +21,6 @@ def extractHistoricalPrices(ticker, directoryName):
     directoryPath = currPath + "/" + directoryPath + "/"
     if !(os.path.exists(directoryPath)):
         os.mkdir(directoryPath)
+	else
+		
     
