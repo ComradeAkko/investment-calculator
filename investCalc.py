347ad9acc83697b5e0de872f39d34ed54e36921c
ComradeAkko <jirojpn2412@gmail.com>
2019-07-03 14:31:12 -0400

    figured out how to sort csv files

--- investCalc.py
+++ investCalc.py
@@ -3,17 +3,9 @@
 #investCalc by ComradeAkko
 
 from priceExtractor import getTreasuryData, getStockData
-from datetime import datetime
-import sys, csv, operator
 
-data = csv.reader(open('stocks\\SPY\\SPY_dividend.csv'), delimiter=',')
-next(data)
-sorted(data, key=lambda day: datetime.strptime(day[0], "%Y-%m-%d"),reverse = False)
+getStockData("MSFT")
 
-#need to make sure to delete previous data
-
-#get a treasury bonds returns data
-#get some federal tax rates data
-#capital gains tax
-#comission fees
-#sorting data by date
+# calculate 200 day moving average
+# calculate 50 day moving average
+# calculate buying and selling
