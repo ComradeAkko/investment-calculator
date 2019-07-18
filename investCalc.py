1c3803e7f0e9a1258bbd31850b63d05026f5a1bc
ComradeAkko <jirojpn2412@gmail.com>
2019-07-18 17:51:33 -0400

    finished buy n hold strat

--- investCalc.py
+++ investCalc.py
@@ -4,19 +4,22 @@
 
 from priceExtractor import getTreasuryData, getStockData, newStockDirectory, newTreasuryDirectory
 from datetime import datetime
-import csv, operator
-import math
+import csv, operator, math, os
+
 
 # Classes:
 
 # stores data for easy returning of data
 class Data:
-    def __init__(self, otherData):
+    def __init__(self):
+        self.initial = 0
         self.assets = 0
         self.cagr = 0
         self.taxes = 0
         self.comissions = 0
         self.pl = 0
+        self.div = 0
+        self.iDate = 0
 
 # records the data for each purchase of the stock
 class Slot:
@@ -31,6 +34,62 @@ class Nlot:
         self.amount = amount
         self.rate = rate
         self.date = date
+
+# returns a boolean based on whether the csv file contains data
+def dataExists(datePath, limit = 1):
+    # opening the file
+    file = open(datePath)
+    data = csv.reader(Path)
+
+    # summing up the number of rows
+    rowCount = sum(1 for row in data)
+
+    # closing the file
+    file.close()
+    
+    # subtracting the header
+    rowCount -= 1
+
+    if rowCount >= limit:
+        return True
+    else:
+        return False
+
+
+# returns a boolean based on whether the csv file contains a date or not
+def timeExists(date, dataPath):
+    file = open(dataPath)
+    data = csv.reader(file)
+
+    # if there are any data points to search dates
+    if dataExists(dataPath):
+        # skipping the header
+        next(data)
+
+        # for every row of data
+        for row in data:
+            # if the row contains the date return True
+            if row[0] == date:
+                file.close()
+                return True
+        
+        # if the for loop did not return True for its entire length,
+        # return False
+        file.close()
+        return False
+    
+# returns the value of whatever dividend paid or stock fraction split
+# assumes the date and value exists
+def getDivSplit(date,dataPath):
+    file = open(dataPath)
+    data = csv.reader(file)
+
+    # cycles through the row till it finds the relevant date
+    for row in data:
+        if row[0] == date:
+            file.close()
+            return row[1]
+
 
 # returns the federal 2019 tax rate based on income
 def fedTax(income):
@@ -49,15 +108,33 @@ def fedTax(income):
     else:
         return .37
 
+# returns the federal 2019 tax rate for dividends
+def divTax(bracket):
+    if bracket <.25:
+        return 0
+    else if bracket < .35:
+        return .15
+    else:
+        return .2
+
 # calculates compound annual growth rate
-def cagr(bb, eb, n):
+def cagr(bb, eb, initialDate, currentDate):
+    # convert csv dates to date time
+    initialDate = datetime.strptime(initialDate, "%Y-%m-%d")
+    currentDate = datetime.strptime(currentDate, "%Y-%m-%d")
+
+    # get the difference in years
+    n = currentDate.year - initialDate.year
+
+    # return cagr
     return (eb^(1/n))/bb - 1
 
 # calculates the current moving average based on the period
 # and the current date
 def movingAverage(period, date, dataPath):
+    file = open(dataPath)
     # get the csv file
-    data = csv.reader(open(dataPath))
+    data = csv.reader(file)
     index = 0
 
     # figure out which index holds the current date
@@ -76,37 +153,103 @@ def movingAverage(period, date, dataPath):
     
     # get the average
     SMA = sum/period
+
+    file.close()
     
     return SMA
 
 
 # returns data for the Buy and Hold strat
-def BH(ticker, baseSMA, cash, commission):
+def BH(ticker, baseSMA, cash, income, commission):
     # initialize the data
     data = Data()
+
+    #set initial cash
+    data.initial = cash
 
     # account for comissions
     cash -= commission
     data.comissions += commission
 
-    #create the file path
+    #create the file paths
     pricePath = os.getcwd() + "\\stocks\\" + ticker + "\\" + ticker + "_price.csv"
+    divPath = os.getcwd() + "\\stocks\\" + ticker + "\\" + ticker + "_dividend.csv"
+    splitPath = os.getcwd() + "\\stocks\\" + ticker + "\\" + ticker + "_split.csv"
     
     # get the csv file
-    prices = csv.reader(open(pricePath))
+    priceF = open(pricePath)
+    prices = csv.reader(priceF)
+    
+    divF = open(divPath)
+    dividends = csv.reader(divF)
 
-    #skip the first 200 days
+    splitF = open(splitPath)
+    splits = csv.reader(splitF)
+
+    # skipping the headers
+    next(prices)
+    next(dividends)
+    next(splits)
+
+    # skip the first 200 days
     for i in range(200)
         next(prices)
 
-        # use a queue?
-            #queue allows for a 
-
-        # use the function?
+    buyRow = next(prices)
     
-
+    # get the opening price for the 201st day
+    price = buyRow[1]
     
+    # get the current date and record it in the data
+    date = buyRow[0]
+    data.iDate = date
 
+    # get the quantity of how many stocks you can buy
+    quantity = math.floor(cash/price)
+
+    # store in the information in a stock lot
+    slot = Slot(price, quantity, date)
+
+    # for every price, check to see if there was a stock split or a dividend issued
+    for row in prices:
+        # if there is any data for splitting stocks
+        if dataExists(splits):
+            # if any of the dates appear in the stock split data
+            if timeExists(row[0],splitPath):
+                # get the fraction of splitting
+                fraction = getDivSplit(row[0], splitPath)
+                slot.quantity = math.floor(slot.quantity / fraction)
+        
+        # if there is any data for dividends
+        if dataExists(dividends):
+            # if any of the dates appear in the dividend data
+            if timeExists(row[0], divPath):
+                # get the dividend for the day and calculate the money gained
+                d = getDivSplit(row[0], divPath)
+                cash += d * slot.quantity * (1 - divTax(fedTax(income)))
+
+                # record the dividends into data
+                data.div += d * slot.quantity * (1 - divTax(fedTax(income)))
+
+                # record the taxes paid
+                data.taxes += d * slot.quantity * divTax(fedTax(income))
+    
+    prices = list(prices)
+    # calculate profit/loss
+    data.pl = (prices[-1][4] - slot.price) * slot.quantity
+
+    # calculate total assets
+    data.assets = cash + (prices[-1][4] * slot.quantity)
+
+    # calculate cagr
+    data.cagr = cagr(data.initial, data.assets, data.iDate, prices[-1][0])
+
+    # close files
+    priceF.close()
+    divF.close()
+    splitF.close()
+
+    return data
 
 
 
@@ -126,13 +269,9 @@ def investCalc(ticker, baseSMA = 200, initial, income, investFrac, aigr, strat, 
     
     # getting the csv file
     stockPricePath = stockPath + "_price.csv"
-    priceData = csv.reader(open(stockPricePath), delimiter = ',')
-
-    # counting the number of data points available
-    rowCount = sum(1 for row in priceData)
 
     # if the there are enough data points for the SMA being used
-    if rowCount > baseSMA:
+    if dataExists(stockPricePath, baseSMA):
         if strat == "MT":
             MTdata = MT(ticker, baseSMA, commission)
             BHdata = BH(ticker, baseSMA, commission)
@@ -160,8 +299,7 @@ def investCalc(ticker, baseSMA = 200, initial, income, investFrac, aigr, strat, 
 
 
 
-# calculate 200 day moving average
-# calculate 50 day moving average
+
 # calculate buying and selling
 
 # rename bonds to notes
